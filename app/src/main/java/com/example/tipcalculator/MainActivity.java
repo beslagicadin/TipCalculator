50d0bc116c8130dcc13441c50635d7710c7d3d4a
Adin Bešlagić <adin.beslagic@asseco-see.ba>
2022-06-09 12:43:20 +0200

    Gradle updates and refactoring

--- app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.tipcalculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,15 +9,16 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText Bill;
     private SeekBar Percent;
-    private Button Calculate;
     private TextView PercentText, Result, Tip;
     private int percentValue;
-    private float billValue;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,16 +26,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Bill = (EditText) findViewById(R.id.bill);
         Percent = (SeekBar) findViewById(R.id.percent);
-        Calculate = (Button) findViewById(R.id.calculate);
+        Button calculate = (Button) findViewById(R.id.calculate);
         PercentText = (TextView) findViewById(R.id.percentText);
         Result = (TextView) findViewById(R.id.result);
-        Tip = (TextView)findViewById(R.id.tip);
-        PercentText.setText(String.valueOf(Percent.getProgress())+"%");
-        Calculate.setOnClickListener(this);
+        Tip = (TextView) findViewById(R.id.tip);
+        PercentText.setText(Percent.getProgress() + "%");
+        calculate.setOnClickListener(this);
         Percent.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                PercentText.setText(String.valueOf(Percent.getProgress())+"%");
+                PercentText.setText(Percent.getProgress() + "%");
             }
 
             @Override
@@ -50,19 +50,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
     }
     @Override
-    public void onClick(View v){
+    public void onClick(View v) {
         calculate();
     }
-    public void calculate(){
-        float res = 0.0f;
-        if(!Bill.getText().toString().equals("")){
-        billValue = Float.parseFloat(String.valueOf(Bill.getText()));
-        res=billValue+Float.valueOf(percentValue*billValue/100);
-        Tip.setText(String.valueOf(percentValue*billValue/100));
-        Result.setText(String.valueOf(res));
-    }
-        else{
-            Toast.makeText(MainActivity.this,"Molim unesite iznos racuna!",Toast.LENGTH_LONG).show();
+    public void calculate() {
+        if (Bill.getText().toString().isEmpty()) {
+            Toast.makeText(MainActivity.this, R.string.billValueEmptyWarning, Toast.LENGTH_SHORT).show();
+        } else {
+            float billValue = Float.parseFloat(String.valueOf(Bill.getText()));
+            float res = billValue + percentValue * billValue / 100;
+            Tip.setText(String.valueOf(percentValue * billValue / 100));
+            Result.setText(String.valueOf(res));
         }
     }
 }
